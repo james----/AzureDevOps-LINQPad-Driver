f3019dad60931bb6ef45bba3d874efc9c2e8862e
Soham Dasgupta <sohdasgupta@deloitte.com>
2018-12-07 02:11:08 +0530

    Added pre-build and post-build commands.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,26 +1,25 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AzureDevOpsDataContextDriver")]
-[assembly: AssemblyDescription("Azure DevOps data context driver")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct("AzureDevOpsDataContextDriver")]
-[assembly: AssemblyCopyright ("")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyDescription("Azure DevOps LINQPad Driver")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Azure DevOps LINQPad Driver")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("73678194-bf89-484f-a938-370b6d47e963")]
+[assembly: Guid("73678194-bf89-484f-a938-370b6d47e963")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,7 +31,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
 
 // The AssemblyFileVersion appears to users when they import Data Context Drivers.
-[assembly: AssemblyFileVersion ("1.0.0.0")]
+//[assembly: AssemblyFileVersion("0.0.0.0")]
